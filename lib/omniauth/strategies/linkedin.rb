24e8b158df3f49e5893ce0a7cc85cce43229ec05
Shiv Shankar <shiva@heurion.com>
2012-08-21 20:16:47 +0530

    rw_nus

--- lib/omniauth/strategies/linkedin.rb
+++ lib/omniauth/strategies/linkedin.rb
@@ -14,7 +14,7 @@ module OmniAuth
 
       option :fields, ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location"]
 
-      option :scope, 'r_basicprofile r_emailaddress'
+      option :scope, 'r_basicprofile r_emailaddress rw_nus'
 
       uid{ raw_info['id'] }
 
